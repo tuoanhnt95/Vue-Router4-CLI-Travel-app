3b140797dd6042c9d8d164303ffbfca299e3baa5
Oanh Nguyen <ONguyen@HTMJPN.local>
2022-10-28 11:23:48 +0900

    refactor(Nav): Change nav link from static to dynamic

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,12 +1,26 @@
 <template>
     <div id="nav">
-        <router-link to="/">Home</router-link>
-        <router-link to="/brazil">Brazil</router-link>
-        <router-link to="/hawaii">Hawaii</router-link>
-        <router-link to="/jamaica">Jamaica</router-link>
-        <router-link to="/panama">Panama</router-link>
+        <router-link
+            v-for="destination in destinations" 
+            :key="destination.id"
+            :to="{name: 'destination.show', params:{ id: destination.id }}"
+        >
+            {{ destination.name }}
+        </router-link>
     </div>
 </template>
+
+<script>
+import sourceData from '@/data.json'
+export default {
+    data() {
+        return {
+            destinations: sourceData.destinations
+        }
+    }
+}
+</script>
+
 <style lang="css">
     #nav .vue-school-active-link {
         color: greenyellow;
