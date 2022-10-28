ea8e7fda959a50c673bef1fe126db3cb66c5e27f
Oanh Nguyen <ONguyen@HTMJPN.local>
2022-10-28 18:07:55 +0900

    feat(forms): add Experience show page

--- src/views/DestinationShowView.vue
+++ src/views/DestinationShowView.vue
@@ -9,11 +9,13 @@
     <section class="experiences">
         <h2>Top Experiences in {{ destination.name }}</h2>
         <div class="cards">       
-            <ExperienceCard 
+            <router-link 
                 v-for="experience in destination.experiences"
                 :key="experience.slug"
-                :experience="experience"
-            />
+                :to="{name: 'experience.show', params:{experienceSlug: experience.slug}}"
+            >
+                <ExperienceCard :experience="experience"/>
+            </router-link>
         </div>
     </section>
 </template>
