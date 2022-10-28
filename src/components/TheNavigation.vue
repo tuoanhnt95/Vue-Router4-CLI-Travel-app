abe13ff8b945601c367d3091969c72a886922a8a
Oanh Nguyen <ONguyen@HTMJPN.local>
2022-10-28 11:41:29 +0900

    refactor(URL): add destination slug in URL

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -6,7 +6,8 @@
         <router-link
             v-for="destination in destinations" 
             :key="destination.id"
-            :to="{name: 'destination.show', params:{ id: destination.id }}"
+            :to="{name: 'destination.show', 
+                params:{ id: destination.id, slug: destination.slug }}"
         >
             {{ destination.name }}
         </router-link>
