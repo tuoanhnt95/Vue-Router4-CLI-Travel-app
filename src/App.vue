cc2d9d94efc226c5d5b743fa42cb6a3a9c0aeb26
Oanh Nguyen <ONguyen@HTMJPN.local>
2022-10-31 17:03:06 +0900

    feat(animations): Add transition fade-in between views

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <TheNavigation/>
   <div class="container">
-    <router-view></router-view>
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" :key="$route.path"></component>
+      </transition>
+    </router-view>
   </div>
 </template>
 
@@ -198,4 +202,15 @@ a {
   border-radius: 5px;
   border:none;
 }
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
 </style>
