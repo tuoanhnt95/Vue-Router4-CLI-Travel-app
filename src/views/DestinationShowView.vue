ca7017eb5e28ed5398d16ceae599f47417555c6a
Oanh Nguyen <ONguyen@HTMJPN.local>
2022-10-31 16:35:26 +0900

    feat(elements): Add go-back button

--- src/views/DestinationShowView.vue
+++ src/views/DestinationShowView.vue
@@ -1,6 +1,7 @@
 <template>
-   <section class="destination">
+    <section class="destination">
         <h1>{{ destination.name }}</h1>
+        <GoBack />
         <div class="destination-details">
             <img :src="`/images/${destination.image}`" :alt="destination.name">
             <p>{{ destination.description }}</p>
@@ -24,10 +25,12 @@
 <script>
 import sourceData from '@/data.json';
 import ExperienceCard from '@/components/ExperienceCard.vue';
+import GoBack from '@/components/GoBack.vue';
 
 export default {    
     components: {
         ExperienceCard,
+        GoBack,
     },
     props: {
         id: {
